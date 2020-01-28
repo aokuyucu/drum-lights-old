5a3392e4133579cc8ece030ba4c9d7add7021de1
aokuyucu <60189675+aokuyucu@users.noreply.github.com>
2020-01-28 14:59:24 -0500

    Code simplification (#2)
    * Update gemma_piezo.ino
    
    Removing unneeded variables and code.
    
    * Update gemma_piezo.ino
    
    Removing large blocks of commented code.
    
    * Update gemma_piezo.ino
    
    Removing unneeded code
    
    * Update gemma_piezo.ino
    
    Final code cleanup

--- gemma_piezo.ino
+++ gemma_piezo.ino
@@ -1,56 +1,41 @@
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        1 // On Trinket or Gemma, suggest changing this to 1
-#define MIC_PIN   A1  // Microphone is attached to Trinket GPIO #2/Gemma D2 (A1)
+#define ANALOG_PIN   A1  // Microphone is attached to Trinket GPIO #2/Gemma D2 (A1)
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 60  //16 // Popular NeoPixel ring size
-#define N_PIXELS  60  // Number of pixels you are using
+#define N_PIXELS  33  // Number of pixels you are using
 
 #define LED_PIN    0  // NeoPixel LED strand is connected to GPIO #0 / D0
 
 // Used for Mic Sensor
-// Delete these next 4 lines?
-#define DC_OFFSET  0  // DC offset in mic signal - if unusure, leave 0
-#define NOISE     100  // Noise/hum/interference in mic signal
+// Delete next line?
 #define SAMPLES   60  // Length of buffer for dynamic level adjustment
-#define TOP       (N_PIXELS +1) // Allow dot to go slightly off scale
 
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
-
-byte
-  peak      = 0,      // Used for falling dot
-  dotCount  = 0,      // Frame counter for delaying dot-falling speed
-  volCount  = 0;      // Frame counter for storing past volume data
+#define DELAYVAL 25 // Time (in milliseconds) to pause between pixels
 
 // Used for Mic Sensor
-// Delete these next 5 lines?
+// Delete next line?
 int
-  vol[SAMPLES],       // Collection of prior volume samples
-  lvl       = 10,     // Current "dampened" audio level
-  minLvlAvg = 0,      // For dynamic adjustment of graph low & high
-  maxLvlAvg = 512;
-
-// Adafruit_NeoPixel  strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+  vol[SAMPLES];       // Collection of prior volume samples
 
 const int threshold = 20;  // threshold value to decide when the detected sound is a knock or not
+const int threshold1 = 20;
+const int threshold2 = 100;
+const int threshold3 = 200;
+const int threshold4 = 300;
 int colorCount = 1;        // counter to determine which color to display next
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
 // strips you might need to change the third parameter -- see the
 // strandtest example for more information on possible values.
-Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // Used for Mic Sensor
-  // Delete these next 2 lines?
-  //memset(vol, 0, sizeof(vol));
-  memset(vol,0,sizeof(int)*SAMPLES);//Thanks Neil!
-
-  Serial.begin(9600);       // use the serial port
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  Serial.begin(9600);  // use the serial port
+  pixels.begin();      // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
 void loop() {
@@ -61,7 +46,7 @@ void loop() {
 
   pixels.clear(); // Set all pixel colors to 'off'
 
-  sensorReading = analogRead(MIC_PIN);          // Raw reading from sensor
+  sensorReading = analogRead(ANALOG_PIN);          // Raw reading from sensor
 
   if (sensorReading >= threshold) {
     Serial.println("Raw sensor reading: ");
@@ -111,77 +96,7 @@ void loop() {
 
   pixels.show(); // Send the updated pixel colors to the hardware.
 
-/*
- *   delay(DELAYVAL);  // delay to avoid overloading the serial port buffer
- *   
-    // For each pixel...
-    for(int i=0; i<NUMPIXELS; i++) {
-      // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-      // Here we're using a moderately bright green color:
-      pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
-      pixels.show();   // Send the updated pixel colors to the hardware.
-
-      delay(DELAYVAL); // Pause before next pass through loop
-    }
-  }
-  else {
-    leds[0] = CRGB::Black;
-    FastLED.show();
-  }
-*/
-
-
-  
-/*
-  n   = abs(sensorReading - 512 - DC_OFFSET);            // Center on zero
-  n   = (n <= NOISE) ? 0 : (n - NOISE);      // Remove noise/hum
-  lvl = ((lvl * 7) + n) >> 3;    // "Dampened" reading (else looks twitchy)
-  
-  // Calculate bar height based on dynamic min/max levels (fixed point):
-  height = TOP * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
-
-  if(height < 0L)       height = 0;      // Clip output
-  else if(height > TOP) height = TOP;
-  if(height > peak)     peak   = height; // Keep 'peak' dot at top
-*/
-
-/*
-  strip.setBrightness(bright);    // Set LED brightness (if POT_PIN at top define commented out, will be full)
-  
-  // Color pixels based on rainbow gradient
-    for(i=0; i<N_PIXELS; i++) {
-      // if(i >= height)
-      if(n >= threshold)
-         strip.setPixelColor(i,   0,   0, 0);
-      else
-         strip.setPixelColor(i,Wheel(map(i,0,strip.numPixels()-1,30,150)));
-    }
-
-   strip.show(); // Update strip
-
-  vol[volCount] = n;                      // Save sample for dynamic leveling
-  if(++volCount >= SAMPLES) volCount = 0; // Advance/rollover sample counter
-
-  // Get volume range of prior frames
-  minLvl = maxLvl = vol[0];
-  for(i=1; i<SAMPLES; i++) {
-    if(vol[i] < minLvl)      minLvl = vol[i];
-    else if(vol[i] > maxLvl) maxLvl = vol[i];
-  }
-  // minLvl and maxLvl indicate the volume range over prior frames, used
-  // for vertically scaling the output graph (so it looks interesting
-  // regardless of volume level).  If they're too close together though
-  // (e.g. at very low volume levels) the graph becomes super coarse
-  // and 'jumpy'...so keep some minimum distance between them (this
-  // also lets the graph go to zero when no sound is playing):
-  if((maxLvl - minLvl) < TOP) maxLvl = minLvl + TOP;
-  minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6; // Dampen min/max levels
-  maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6; // (fake rolling average)
-
-  // delay(1000);  // delay to avoid overloading the serial port buffer
-  delay(500);  // delay to avoid overloading the serial port buffer
-*/
+  delay(DELAYVAL);  // delay to avoid overloading the serial port buffer
 }
 
 // Input a value 0 to 255 to get a color value.
