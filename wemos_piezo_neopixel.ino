8524f87f5fa6dddf287d6ef51e8756668368bbd5
Alp Okuyucu <60189675+aokuyucu@users.noreply.github.com>
2020-03-02 21:44:04 -0500

    Added flicker and chase methods

--- wemos_piezo_neopixel.ino
+++ wemos_piezo_neopixel.ino
@@ -14,7 +14,7 @@ const int threshold2 = 65;
 const int threshold3 = 100;
 const int threshold4 = 200;
 const int threshold5 = 300;
-const int threshold = threshold2;  // threshold value to decide when the detected sound is a knock or not
+const int threshold = threshold1;  // threshold value to decide when the detected sound is a knock or not
 
 int colorCount = 1;        // counter to determine which color to display next
 uint8_t  bright = 255;
@@ -32,18 +32,34 @@ void setup() {
   pixels.begin();      // INITIALIZE NeoPixel strip object (REQUIRED)
   
   pixels.clear(); // Set all pixel colors to 'off'
-  
+
+  // Chase
+  chase(0, 0, bright);
+/*
   for (i=0; i<N_PIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 0, bright));
+  pixels.setPixelColor(i, pixels.Color(0, 0, bright));
     pixels.show();
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     delay(30);
   }
 
+  // All on for 2 seconds
   for (i=0; i<N_PIXELS; i++)
     pixels.setPixelColor(i, pixels.Color(0, 0, bright));
   pixels.show(); // Send the updated pixel colors to the hardware.
-  delay(2000);  // delay to avoid overloading the serial port buffer
+  delay(2000);
+*/
+  shortFlicker(0, 0, bright);
+  shortFlicker(0, 0, bright);
+  shortFlicker(0, 0, bright);
+  mediumFlicker(0, 0, bright);
+  longFlicker(0, 0, bright);
+
+  // All on for 3 seconds
+  for (i=0; i<N_PIXELS; i++)
+    pixels.setPixelColor(i, pixels.Color(0, 0, bright));
+  pixels.show(); // Send the updated pixel colors to the hardware.
+  delay(3000);
 }
 
 void loop() {
@@ -106,6 +122,41 @@ void loop() {
   pixels.show(); // Send the updated pixel colors to the hardware.
 
   // delay(DELAYVAL);  // delay to avoid overloading the serial port buffer
+}
+
+
+void chase(uint8_t redValue, uint8_t greenValue, uint8_t blueValue) {
+  uint8_t  i;
+  for (i=0; i<N_PIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(redValue, greenValue, blueValue));
+    pixels.show();
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    delay(30);
+  }
+}
+
+void shortFlicker(uint8_t red, uint8_t green, uint8_t blue) {
+  flicker(100, 25, red, green, blue);
+}
+
+void mediumFlicker(uint8_t red, uint8_t green, uint8_t blue) {
+  flicker(300, 25, red, green, blue);
+}
+
+void longFlicker(uint8_t red, uint8_t green, uint8_t blue) {
+  flicker(500, 50, red, green, blue);
+}
+
+void flicker(uint8_t onMS, uint8_t offMS, uint8_t red, uint8_t green, uint8_t blue) {
+  uint8_t  i;
+  for (i=0; i<N_PIXELS; i++)
+    pixels.setPixelColor(i, pixels.Color(red, green, blue));
+  pixels.show();
+  delay(onMS);
+  
+  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.show();
+  delay(offMS);
 }
 
 // Input a value 0 to 255 to get a color value.
